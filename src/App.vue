460d9d93ccf6c84b8a63760e226659d234dfc709
SABELOMAWELA <Mawelas63@gmail.com>
2024-08-01 14:00:36 +0200

    "Added axios import and fetchData method to App.vue"

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
-<script> 
+<script>
+import axios from 'axios';
 
-</Script>
+export default {
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios.get('https://fakestoreapi.com/products')
+        .then(res => {
+          console.log(res);
+        })
+        .catch(error => {
+          console.error("There was an error fetching the data:", error);
+        });
+    }
+  }
+};
+</script>
