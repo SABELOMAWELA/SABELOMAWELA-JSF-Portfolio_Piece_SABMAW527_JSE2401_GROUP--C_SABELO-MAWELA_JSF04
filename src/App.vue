c500d9ed88e774c71e1be6ab78d2fdae1fef1f33
SABELOMAWELA <Mawelas63@gmail.com>
2024-08-02 15:05:08 +0200

    "Updated App.vue and cards.vue components to fetch and display product data from API"

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,38 @@
 <script>
 import axios from 'axios';
-import navbar from './components/navbar.vue'
+import navbar from './components/navbar.vue';
 import cards from './components/cards.vue';
+
 export default {
   components: {
-  navbar,
-  cards
+    navbar,
+    cards,
+  },
+  data() {
+    return {
+      products: [],
+    };
   },
   mounted() {
     this.fetchData();
   },
-  
   methods: {
     fetchData() {
       axios.get('https://fakestoreapi.com/products')
         .then(res => {
-          console.log(res);
+          this.products = res.data;
         })
         .catch(error => {
           console.error("There was an error fetching the data:", error);
         });
-    }
-  }
+    },
+  },
 };
-
-
-
 </script>
+
 <template>
   <div id="app">
     <navbar />
-    <cards />
-    <!-- Other content of your app -->
+    <cards :products="products" />
   </div>
 </template>
--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,36 +1,36 @@
 <template>
-    <div
-      onClick={handleClick}
-      class="flex flex-col max-h-[130rem]  cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
+  <div>
+    <div v-for="product in products" :key="product.id"
+      @click="handleClick"
+      class="flex flex-col max-h-[130rem] cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
     >
-      <img class="object-contain h-48 mt-3"  alt="Course 01" />
+      <img :src="product.image" class="object-contain h-48 mt-3" :alt="product.title" />
 
       <div class="flex-1 flex flex-col p-6">
         <div class="flex-1">
-   
           <header class="mb-2 flex-2">
             <h2 class="text-lg line-clamp-2 font-extrabold leading-snug">
               <div class="text-slate-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300">
-            
+                {{ product.title }}
               </div>
             </h2>
           </header>
-          <Ratings {...rating} />
+          
           <div class="text-base line-clamp-2 font-extrabold text-slate-500 leading-snug">
-            <h2></h2>
+            <h2>{{ product.description }}</h2>
           </div>
         </div>
 
         <div class="flex mt-1 space-x-2">
           <div class="justify-start flex-1">
             <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-              {category}
+              ${{ product.price }}
             </span>
           </div>
           <div class="justify-end space-x-2">
-            <button onClick={addToFavourites}>
+            <button @click="addToFavourites">
               <svg
-                class="me-1.5 h-5 w-5 hover:fill-red-500 "
+                class="me-1.5 h-5 w-5 hover:fill-red-500"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -56,12 +56,25 @@
         </div>
       </div>
     </div>
-        
-
+  </div>
 </template>
 
 <script>
 export default {
   name: 'cards',
+  props: {
+    products: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    handleClick() {
+      // Handle click event
+    },
+    addToFavourites() {
+      // Handle add to favourites event
+    }
+  }
 };
 </script>
