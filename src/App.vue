2e864320166eae9e45a30f03ade3aad41a507eac
SABELOMAWELA <Mawelas63@gmail.com>
2024-08-05 14:51:42 +0200

    "Added filtering, sorting, and searching functionality to App.vue"

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,22 @@
 import axios from 'axios';
 import navbar from './components/navbar.vue';
 import cards from './components/cards.vue';
-import { ref, onMounted } from 'vue';
+import FilterSortComponent from './components/FilterSortComponent.vue';
+import { ref, onMounted} from 'vue';
 
 const products = ref([]);
+const filteredProducts = ref([]);
+const categories = ref([]);
+const searchTerm = ref('');
+const selectedCategory = ref('');
+const sorting = ref('');
 
 function fetchData() {
   axios.get('https://fakestoreapi.com/products')
     .then(res => {
       products.value = res.data;
+      filteredProducts.value = res.data;
+      categories.value = [...new Set(res.data.map(product => product.category))];
     })
     .catch(error => {
       console.error("There was an error fetching the data:", error);
@@ -17,11 +25,54 @@ function fetchData() {
 }
 
 onMounted(() => fetchData());
+
+function filterProducts() {
+  let tempProducts = [...products.value];
+
+  if (selectedCategory.value) {
+    tempProducts = tempProducts.filter(product => product.category === selectedCategory.value);
+  }
+
+  if (searchTerm.value) {
+    tempProducts = tempProducts.filter(product => product.title.toLowerCase().includes(searchTerm.value.toLowerCase()));
+  }
+
+  if (sorting.value) {
+    if (sorting.value === 'Price: low to high') {
+      tempProducts.sort((a, b) => a.price - b.price);
+    } else if (sorting.value === 'Price: high to low') {
+      tempProducts.sort((a, b) => b.price - a.price);
+    }
+  }
+
+  filteredProducts.value = tempProducts;
+}
+
+function handleFilter(category) {
+  selectedCategory.value = category;
+  filterProducts();
+}
+
+function handleSearch(term) {
+  searchTerm.value = term;
+  filterProducts();
+}
+
+function handleSort(order) {
+  sorting.value = order;
+  filterProducts();
+}
 </script>
 
 <template>
   <div id="app">
     <navbar />
-    <cards :products="products" />
+    <FilterSortComponent 
+      :categories="categories" 
+      @filter="handleFilter" 
+      @search="handleSearch" 
+      @sort="handleSort" 
+    />
+    <cards :products="filteredProducts" />
   </div>
 </template>
