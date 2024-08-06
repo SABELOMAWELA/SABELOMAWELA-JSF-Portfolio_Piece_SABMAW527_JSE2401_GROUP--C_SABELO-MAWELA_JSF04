cc681c10122088aa5f2da5aef199a9be3381334a
SABELOMAWELA <Mawelas63@gmail.com>
2024-08-06 14:17:55 +0200

    init

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -4,7 +4,7 @@
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <span>
           <button class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="" class="h-8" alt="Flowbite Logo" />
+            <img src="../assets/online-shop.png" class="h-8" alt="Flowbite Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">
               SwiftCart
             </span>
